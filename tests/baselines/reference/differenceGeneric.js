3b442421fdf3e6f223fa981036d7bfa11f5af9e0
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-11-02 10:48:14 -0700

    Update/add baselines

--- tests/baselines/reference/differenceGeneric.js
+++ tests/baselines/reference/differenceGeneric.js
@@ -30,14 +30,6 @@ rested.w;
 
 
 //// [differenceGeneric.js]
-var __assign = (this && this.__assign) || Object.assign || function(t) {
-    for (var s, i = 1, n = arguments.length; i < n; i++) {
-        s = arguments[i];
-        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-            t[p] = s[p];
-    }
-    return t;
-};
 var __rest = (this && this.__rest) || function (s, e) {
     var t = {};
     for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && !e.indexOf(p))
